9f6d920f83caccc0e3b7069f622a667b3846d7be
Letouche Jeremy <jeremyletouche180796@gmail.com>
2020-06-15 11:01:08 +0200

    teest

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+{
+  "name": "be.jerem.keystrokeutility",
+  "displayName": "jeremToolBox",
+  "version": "0.0.1",
+  "unity": "2018.1",
+  "description": "You package description",
+  "keywords": ["Script","Tool"],
+  "category": "Script",
+  "dependencies":{}
+}
